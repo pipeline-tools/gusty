6e3b1cabe49934b553eb3ac3ac12ec354334fde4
peppato <cfcardillo23@gmail.com>
2020-02-27 15:06:45 -0500

    Now with GustyDAG

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gusty",
-    version="0.0.4",
+    version="0.0.5",
     author="Chris Cardillo",
     author_email="cfcardillo23@gmail.com",
     description="An opinionated framework for ETL built on top of Airflow",
