79ccbbd72a3408e7ee2f6c736dc52dcba28d57e5
chriscardillo <cfcardillo23@gmail.com>
2020-12-22 16:42:59 -0500

    github actions

--- gusty/tests/test_operators.py
+++ gusty/tests/test_operators.py
@@ -6,12 +6,6 @@ from pathlib import Path
 
 import pytest
 
-# will set in travis, but adding a default just in case
-os.environ.setdefault(
-    "AIRFLOW_CONN_POSTGRES_DEFAULT",
-    "postgresql://postgres:@localhost:5433/postgres"
-    )
-
 @pytest.fixture
 def tmp_dir():
     # pandas used to create a CSV, but could use python's built in csv module
@@ -21,19 +15,8 @@ def tmp_dir():
     orig_home = os.environ.get("AIRFLOW_HOME")
     with tempfile.TemporaryDirectory() as tmp_dir:
         # set new env variables
-        os.environ["AIRFLOW_HOME"] = tmp_dir
-
-        # create csv directory
-        csv_dir = Path(tmp_dir) / 'dags' / 'csv'
-        csv_dir.mkdir(parents=True, exist_ok=False)
-
+        os.environ["AIRFLOW_HOME"] = orig_home or tmp_dir
         yield tmp_dir
-
-    # there is a better way to ensure we reset to original values
-    if orig_home is not None:
-        os.environ["AIRFLOW_HOME"] = orig_home
-
-
 
 def test_stub():
     assert True
