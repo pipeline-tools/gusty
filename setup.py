d2b185ec7e905eb8cbf9d5eeb1072717dfcf3540
chriscardillo <cfcardillo23@gmail.com>
2021-02-18 11:47:21 -0500

    adding gusty_home as option for operator location

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gusty",
-    version="0.5.0",
+    version="0.5.1",
     author="Chris Cardillo",
     author_email="cfcardillo23@gmail.com",
     description="Making DAG construction easier",
