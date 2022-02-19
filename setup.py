c4c66c066ca2839b5b58d2438af178002bbadcb1
chriscardillo <cfcardillo23@gmail.com>
2022-02-19 17:24:28 -0500

    black format setup.py

--- setup.py
+++ setup.py
@@ -13,17 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/chriscardillo/gusty",
     packages=setuptools.find_packages(),
-    install_requires=[
-          'apache-airflow',
-          'inflection',
-          'jupytext',
-          'nbformat',
-          'PyYaml'
-    ],
+    install_requires=["apache-airflow", "inflection", "jupytext", "nbformat", "PyYaml"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
