c314a3ac53df1f1d8715ab344dbf696a62f73e89
chriscardillo <cfcardillo23@gmail.com>
2022-06-02 19:34:49 -0400

    use frontmatter_load from absql

--- gusty/parsing/parsers.py
+++ gusty/parsing/parsers.py
@@ -1,40 +1,18 @@
 import yaml, ast, importlib.util, nbformat, jupytext, re
 from gusty.parsing.loaders import generate_loader
 from gusty.importing import airflow_version
+from absql.files.parsers import frontmatter_load
 
 if airflow_version > 1:
     from airflow.operators.python import PythonOperator
 else:
     from airflow.operators.python_operator import PythonOperator
 
-
-def frontmatter_load(file_path, loader=None):
-    """
-    Loads YAML frontmatter. Expects a YAML block at the top of the file
-    that starts and ends with "---". In use in favor of frontmatter.load
-    so that custom dag_constructors (via PyYaml) can be used uniformly across
-    all file types.
-    """
-    if loader is None:
-        loader = generate_loader()
-    FM_BOUNDARY = re.compile(r"^-{3,}\s*$", re.MULTILINE)
-    with open(file_path, "r") as file:
-        text = "".join(file.readlines())
-        if text.startswith("---"):
-            _, metadata, content = FM_BOUNDARY.split(text, 2)
-            metadata = yaml.load(metadata, Loader=loader)
-            content = content.strip("\n")
-        else:
-            metadata = None
-            content = yaml.load(text, Loader=loader)
-    return {"metadata": metadata, "content": content}
-
-
 def parse_generic(file_path, loader=None):
     if loader is None:
         loader = generate_loader()
     # Read either the frontmatter or the parsed yaml file (using "or" to coalesce them)
-    file_contents = frontmatter_load(file_path)
+    file_contents = frontmatter_load(file_path, loader=loader)
     job_spec = file_contents["metadata"] or file_contents["content"]
 
     return job_spec
