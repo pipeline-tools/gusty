6107c2db02363ce2449a572454a49fa2a8418d33
chriscardillo <cfcardillo23@gmail.com>
2022-06-02 22:35:34 -0400

    pull out default_constructors; handler_user_constructors

--- gusty/parsing/loaders.py
+++ gusty/parsing/loaders.py
@@ -1,17 +1,18 @@
 import yaml
-from absql.files.loader import scalar_to_value, node_converter, wrap_yaml
 from absql.files.loader import generate_loader as generate_absql_loader
 from datetime import datetime, timedelta
 from gusty.utils import days_ago
 
+default_constructors = {
+    "!days_ago": days_ago,
+    "!timedelta": timedelta,
+    "!datetime": datetime,
+}
+
 
 def generate_loader(custom_constructors={}):
     """Generates a SafeLoader with both standard Airflow and custom constructors"""
-    dag_yaml_tags = {
-        "!days_ago": days_ago,
-        "!timedelta": timedelta,
-        "!datetime": datetime,
-    }
+    dag_yaml_tags = default_constructors.copy()
 
     if isinstance(custom_constructors, list) and len(custom_constructors) > 0:
         custom_constructors = {
@@ -22,3 +23,9 @@ def generate_loader(custom_constructors={}):
         dag_yaml_tags.update(custom_constructors)
 
     return generate_absql_loader(dag_yaml_tags)
+
+
+def handle_user_constructors(user_constructors):
+    if isinstance(user_constructors, list):
+        user_constructors = {("!" + func.__name__): func for func in user_constructors}
+    return user_constructors
