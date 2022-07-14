c005fa46b7417f2e7330fbce78a1d8ad26158f6c
chriscardillo <cfcardillo23@gmail.com>
2022-07-14 19:16:24 -0400

    Informative timeout error

--- gusty/experimental.py
+++ gusty/experimental.py
@@ -11,11 +11,22 @@ async def create_dag_async(dag_dir, **kwargs):
 
 
 async def create_dags_async(gusty_dags, globals, timeout, **kwargs):
-    async_dags = [create_dag_async(gusty_dag, **kwargs) for gusty_dag in gusty_dags]
+    # loop instead of listcomp so we can (hopefully) attach
+    #  the dag name to the async function in the future
+    async_dags = []
+    for gusty_dag in gusty_dags:
+        async_func = create_dag_async(gusty_dag, **kwargs)
+        async_dags.append(async_func)
+    # async_dags = [create_dag_async(gusty_dag, **kwargs) for gusty_dag in gusty_dags]
     async_results = asyncio.as_completed(async_dags, timeout=timeout)
     for async_dag in async_results:
-        dag_dict = await async_dag
-        globals[dag_dict["dag_id"]] = dag_dict["dag"]
+        try:
+            dag_dict = await async_dag
+            globals[dag_dict["dag_id"]] = dag_dict["dag"]
+        except asyncio.exceptions.TimeoutError:
+            raise asyncio.exceptions.TimeoutError(
+                "create_dags took more than " + str(timeout) + " seconds."
+            )
 
 
 def create_dags(dags_dir, globals, parallel=False, timeout=None, **kwargs):
